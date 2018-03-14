af439c688ad84c06dce4e691ec013c9385637e22
kstanaszek <k.stanaszek@gmail.com>
2018-03-14 22:37:05 +0100

    message service and test added

--- src/app/message/message.service.ts
+++ src/app/message/message.service.ts
@@ -15,23 +15,40 @@ export class MessagesService {
     messages: Observable<Message[]>;
     updates: Subject<any> = new Subject<any>();
 
+    // actions streams
     create: Subject<Message> = new Subject<Message>();
+    markThreadAsRead: Subject<any> = new Subject<any>();
 
-    constructor(){
+    constructor() {
         this.messages = this.updates
-        .scan((messages: Message[], operation: IMessagesOperation) => {
-            return operation(messages);
-        }, initialMessages)
-        .publishReplay(1)
-        .refCount()
+            .scan((messages: Message[], operation: IMessagesOperation) => {
+                return operation(messages);
+            }, initialMessages)
+            .publishReplay(1)
+            .refCount()
 
         this.create
-        .map(function(message: Message): IMessagesOperation {
-            return (messages: Message[]) => {
-                return messages.concat(message);
-            };
-        })
-        .subscribe(this.updates)
+            .map(function (message: Message): IMessagesOperation {
+                return (messages: Message[]) => {
+                    return messages.concat(message);
+                };
+            })
+            .subscribe(this.updates);
+
+        this.newMessages
+            .subscribe(this.create);
+
+        this.markThreadAsRead
+            .map((thread: Thread) => {
+                return (messages: Message[]) => {
+                    return messages.map((message: Message) => {
+                        if (message.thread.id === thread.id) {
+                            message.isRead = true;
+                        }
+                        return message;
+                    });
+                };
+            })
     }
 
     addMessage(newMessage: Message): void {
@@ -42,9 +59,9 @@ export class MessagesService {
 
     messagesForThreadUser(thread: Thread, user: User): Observable<Message> {
         return this.newMessages
-        .filter((message: Message) => {
-            return (message.thread.id === thread.id) && 
-            (message.author.id != user.id)
-        })
+            .filter((message: Message) => {
+                return (message.thread.id === thread.id) &&
+                    (message.author.id != user.id)
+            })
     }
 }
