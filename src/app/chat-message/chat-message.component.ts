6bdfb157658cb87df9b77b8565ba3a490c479535
Krzysztof Stanaszek <kstanaszek@objectivity.co.uk>
2018-04-06 08:26:54 +0200

    chat messages implementation added

--- src/app/chat-message/chat-message.component.ts
+++ src/app/chat-message/chat-message.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Message } from '../message/message.model';
+import { User } from '../user/user.model';
+import { UsersService } from '../user/users.service';
 
 @Component({
   selector: 'app-chat-message',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-message.component.css']
 })
 export class ChatMessageComponent implements OnInit {
+  @Input() message: Message;
+  currentUser: User;
+  incoming: boolean;
 
-  constructor() { }
+  constructor(public usersService: UsersService) { }
 
   ngOnInit() {
+    this.usersService.currentUser.subscribe(
+      (user: User) => {
+        this.currentUser = user;
+        if(this.message.author && user){
+          this.incoming = this.message.author.id !== user.id;
+        }
+      }
+    )
   }
 
 }
